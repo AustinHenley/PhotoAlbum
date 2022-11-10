b1495d45ffe72aa932e75292450d89922ea79414
Austin Henley <haustin589@gmail.com>
2022-11-09 22:56:28 -0600

    Cleaned code

--- PhotoAlbum/Models/Photo.cs
+++ PhotoAlbum/Models/Photo.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using RestSharp;
+
 namespace PhotoAlbum.Models
 {
     public class Photo
     {
         public int albumId { get; set; }
         public int id { get; set; }
-        public string title { get; set; }
-        public string url { get; set; }
-        public string thumbnailUrl { get; set; }
+        public string title { get; set; } = "";
+        public string url { get; set; } = "";
+        public string thumbnailUrl { get; set; } = "";
 
-        public Photo()
-        {
+        public static List<Photo> PullPhotos() {
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest($"photos");
+            return client.Get<List<Photo>>(request) ?? new List<Photo>();
         }
     }
 }
-
